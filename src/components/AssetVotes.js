c9ae7166d243bfdbf879a8fa8a6ae45d359985f8
Mike <mostlyfocusedmike@gmail.com>
2018-11-14 13:50:13 -0500

    get votes working rendering optimistically

--- src/components/AssetVotes.js
+++ src/components/AssetVotes.js
@@ -1,13 +1,39 @@
 import React from 'react'
 
-const AssetVotes = (props) => {
-    return (
-        <form id="asset-votes">
-            <button id="vote-up">Up</button>
-            {props.asset.votes}
-            <button id="vote-up">Down</button>
-        </form>
-    )
+class AssetVotes extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            votes: props.asset.votes,
+            voteValue: 0
+        }
+    }
+    handleVote = (e) => {
+        e.preventDefault();
+        const num = parseInt(e.target.dataset.count)
+        const voteCheck = this.state.voteValue + num;
+        if (Math.abs(voteCheck) > 1) {
+            return false;
+        }
+        this.setState((prevState) => {
+            return {
+                votes: prevState.votes + num,
+                voteValue:  prevState.voteValue + num,
+            }
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    render() {
+        return (
+            <form id="asset-votes" onSubmit={this.handleSubmit}>
+                <button id="vote-up" data-count="1" onClick={this.handleVote}>Up</button>
+                {this.state.votes}
+                <button id="vote-up" data-count="-1" onClick={this.handleVote}>Down</button>
+            </form>
+        )
+    }
 }
 
 export default AssetVotes;
