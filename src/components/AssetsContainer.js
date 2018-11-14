45d968b3998c27687c24d8e8ffabf1c3d06a95b8
Mike <mostlyfocusedmike@gmail.com>
2018-11-14 10:15:42 -0500

    build dummy db and asset container

---
+++ src/components/AssetsContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class AssetsContainer extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div id="assets-container">
+                <h1>I am the Assets Container</h1>
+            </div>
+        )
+    }
+}
+
+export default AssetsContainer
